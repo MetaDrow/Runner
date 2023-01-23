72ff53d6549fa48a612ac9584961b3c717cfab25
MetaDrow <brinteraw@gmail.com>
2023-01-23 13:17:00 +0300

    Minor chanches

--- Assets/Scripts/Character/Jump.cs
+++ Assets/Scripts/Character/Jump.cs
@@ -3,19 +3,24 @@ using UnityEngine;
 public class Jump : MonoBehaviour
 {
     [SerializeField]private float jumpForce = 2f;
-
+    private bool isGrounded;
     private Rigidbody _rb;
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
     }
 
+    private void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true;
+    }
     private void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
 
-            _rb.velocity = Vector3.up * jumpForce;
+            _rb.velocity = (Vector3.up * jumpForce);
+            isGrounded = false;
         }
     }
 
