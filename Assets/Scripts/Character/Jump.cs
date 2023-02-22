1fccd64ebe1bf239cd1963aaa2955fbd8e0a0359
MetaDrow <106996152+MetaDrow@users.noreply.github.com>
2023-02-22 20:13:30 +0300

    new character,animation, some enviroment prefabs

--- Assets/Scripts/Character/Jump.cs
+++ Assets/Scripts/Character/Jump.cs
@@ -27,16 +27,14 @@ public class Jump : MonoBehaviour
             _isGround= false;
         }
 
-        /*
+        
         if (_rb.position.y > 1 ) //out of if 
         {
-            Vector3 gravity = new Vector3(0, -gravityScale, 0);
-            _rb.velocity = gravity;
 
-           // _anim.Play("Fall");
+            _anim.Play("Fall");
 
         }
-        */
+        
         if (Input.GetKeyDown(KeyCode.S))
         {
             _anim.Play("Roll");
