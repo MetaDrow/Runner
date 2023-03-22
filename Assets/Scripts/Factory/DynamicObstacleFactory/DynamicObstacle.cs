56c790e973333eacddf34fa202701e51a52a2c3d
MetaDrow <106996152+MetaDrow@users.noreply.github.com>
2023-03-22 14:40:46 +0300

    Add DynamicObstacles

---
+++ Assets/Scripts/Factory/DynamicObstacleFactory/DynamicObstacle.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicObstacle : MonoBehaviour
+{
+    public GameObject _DynamicObstacle;
+    public Collider _collider;
+    void Start()
+    {
+        _DynamicObstacle.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // OnTrigeredObstacle( _collider);
+    }
+
+    public void OnTrigeredObstacle(Collider _collider)
+    {
+        if (CompareTag("DynamicObstacleTrigger") && _collider.CompareTag("Player"))
+        {
+            _DynamicObstacle.SetActive(true);
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (CompareTag("DynamicObstacleTrigger") && other.CompareTag("Player"))
+        {
+            _DynamicObstacle.SetActive(true);
+
+        }
+
+    }
+}
